c42abdebdddc5a6e3d9c2d9293128cc1d91678cf
Pratham Wanmode <prathamwanmode3012@gmail.com>
2021-02-28 20:02:35 +0530

    Initial commit

---
+++ app/src/main/java/mibtechn/opticalstorm/client/helper/VolleyCallback.java
@@ -0,0 +1,7 @@
+package mibtechn.opticalstorm.client.helper;
+
+public interface VolleyCallback {
+    void onSuccess(boolean result, String message);
+    //void onSuccessWithMsg(boolean result, String message);
+
+}
